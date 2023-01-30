985b95773103585b717e16edc562cf1cb6a41e13
Yarukon <a2692866561@live.com>
2023-01-30 23:47:54 +0800

    some note

--- BossMod/Framework/Utils.cs
+++ BossMod/Framework/Utils.cs
@@ -110,7 +110,7 @@ namespace BossMod
         public static unsafe byte CharacterAnimationState(Character chr, bool second) => ReadField<byte>(CharacterInternal(chr), second ? 0x1ADD : 0x1ADC); // see actor control 62 // 41 88 86 ? ? ? ? E8 ? ? ? ? 0F 28 CE (second, first one just second - 1)
         public static unsafe byte CharacterModelState(Character chr) => ReadField<byte>(CharacterInternal(chr), 0x1ADE); // see actor control 63 // 41 88 86 ? ? 00 00 49 8B CE 41 0F B6 ?
         public static unsafe float CharacterCastRotation(Character chr) => ReadField<float>(CharacterInternal(chr), 0x1A84); // see ActorCast -> Character::StartCast // F3 0F 11 97 ? ? ? ? F6 43
-        public static unsafe ulong CharacterTargetID(Character chr) => ReadField<ulong>(CharacterInternal(chr), 0x1A68); // until FFXIVClientStructs fixes offset and type... // 48 89 AB ? ? ? ? 48 89 AB ? ? ? ? 89 AB ? ? ? ? 48 89 B3
+        public static unsafe ulong CharacterTargetID(Character chr) => ReadField<ulong>(CharacterInternal(chr), 0x1A68); // until FFXIVClientStructs fixes offset and type... // 40 53 48 83 EC ? 45 33 C0 B2 ? 48 8B D9 E8 ? ? ? ? 84 C0 74 ? 48 8D 0D ? ? ? ? 48 83 C4 ? 5B E9 ? ? ? ? 48 8B 83 68 1A 00 00 - return (a1 + offset)
         public static unsafe byte CharacterTetherID(Character chr) => ReadField<byte>(CharacterInternal(chr), 0x1A00); // see ActorControl -> Tether -> Character::SetTether (note that there is also a secondary tether...)
         public static unsafe ulong CharacterTetherTargetID(Character chr) => ReadField<ulong>(CharacterInternal(chr), 0x1A10);
         public static unsafe Vector3 BattleCharaCastLocation(BattleChara chara) => BattleCharaInternal(chara)->SpellCastInfo.CastLocation; // see ActorCast -> Character::StartCast -> Character::StartOmen
